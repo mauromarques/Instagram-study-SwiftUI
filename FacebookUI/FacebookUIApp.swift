c90d6d65c3bd7d3c566401533ca38181d6516dc4
Mauro <mmcanhao@gmail.com>
2020-09-23 07:49:27 +0100

    Initial Commit

---
+++ FacebookUI/FacebookUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FacebookUIApp.swift
+//  FacebookUI
+//
+//  Created by Mauro Canhao on 23/09/2020.
+//
+
+import SwiftUI
+
+@main
+struct FacebookUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
